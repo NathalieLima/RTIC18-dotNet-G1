e767faa7ffafda4a793d6e064a5d04a967b320c2
daniel <danieloliveira.s558@gmail.com>
2023-12-15 09:41:13 -0300

    Finalizado atividade alterei o namespace para praticar

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,11 +1,12 @@
 using Semana1.Domain;
+using ClassDaniel.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/user/", () => User.View());
-app.MapGet("/lorena/", () => Lorena.View());//Rota lorena
 
-app.MapGet("/daniel", () => Daniel.ViewProfile());
+app.MapGet("/lorena/", () => Lorena.View());//Rota lorena
+app.MapGet("/daniel/", () => Daniel.ViewProfile());
 app.Run();
