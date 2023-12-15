bce1c2c3767d4d1731cf2c84eb65a40309ae93a4
Lorena Andrade <lorenaandrade@gmail.com>
2023-12-15 16:21:16 -0300

    rota principal com resumo e totalizacao de estrelas

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,25 +1,32 @@
 using Semana1.Domain;
 using ClassDaniel.Domain;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-string Profiles()
-{
-    return AlanPrates.View() + Lorena.View() + Daniel.ViewProfile();
-}
 
-string ListarHabilidadesAlanPrates()
-{
-    return AlanPrates.ListarHabilidades();
-}
+app.MapGet("/", () => {
+    var strRotaPrincipal = new StringBuilder();
+    strRotaPrincipal.AppendLine("Resumo das habilidades da equipe:");
+    strRotaPrincipal.AppendLine();
+    //O resumo de cada membro
+    strRotaPrincipal.AppendLine(Lorena.View());
+    strRotaPrincipal.AppendLine(Daniel.ViewProfile());
+    strRotaPrincipal.AppendLine(AlanPrates.ListarHabilidades());
 
-app.MapGet("/", () => Profiles());
+    // Soma geral da equipe
+    var totalGeral = Lorena.TotalEstrelas + Daniel.TotalEstrelas + AlanPrates.TotalEstrelas;
+    strRotaPrincipal.AppendLine();
+    strRotaPrincipal.AppendLine($"Soma geral da equipe: {totalGeral}");
+
+    return strRotaPrincipal.ToString();
+});
 
 app.MapGet("/lorena/", () => Lorena.View());
 
 app.MapGet("/daniel/", () => Daniel.ViewProfile());
 
-app.MapGet("/alanprates", () => ListarHabilidadesAlanPrates());
+app.MapGet("/alanprates", () => AlanPrates.ListarHabilidades());
 
 app.Run();
