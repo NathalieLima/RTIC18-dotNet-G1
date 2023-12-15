cd4df1a43b410f1d0af737e1c786750593a38d69
NathalieLima <nathalielima3@gmail.com>
2023-12-15 17:21:35 -0300

    Estrutura padrão para camada Domain

---
+++ ResTIConnect/ResTIConnect.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,16 @@
+using ResTIConnect.Domain.Common;
+
+namespace ResTIConnect.Domain.Interfaces;
+
+public interface BaseRepository<T> where T : BaseEntity
+{
+    void Create(T entity);
+
+    void Update(T entity);
+
+    void Delete(T entity);
+
+    Task<T> Get(Guid id, CancellationToken cancellationToken);
+
+    Task<List<T>> GetAll(CancellationToken cancellationToken);
+}
