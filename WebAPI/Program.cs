e1718947ee78bcc3af2a5b06827d196d0813d4bd
daniel <danieloliveira.s558@gmail.com>
2023-12-15 10:29:57 -0300

    Task 3 concluida

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,10 +1,17 @@
 using Semana1.Domain;
+using ClassDaniel.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/user/", () => User.View());
-app.MapGet("/lorena/", () => Lorena.View());//Rota lorena
+string Profiles(){
 
+    return User.View() + Lorena.View() + Daniel.ViewProfile();
+}
+
+app.MapGet("/", () => Profiles());
+app.MapGet("/user/", () => User.View());
+
+app.MapGet("/lorena/", () => Lorena.View());//Rota lorena
+app.MapGet("/daniel/", () => Daniel.ViewProfile());
 app.Run();
