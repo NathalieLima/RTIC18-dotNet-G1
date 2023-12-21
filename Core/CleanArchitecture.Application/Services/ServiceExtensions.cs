5523df5aeab626a3e82ac8f4bce9bbe58469a2ff
Lorena Andrade <lorenaandrade@gmail.com>
2023-12-21 16:56:14 -0300

    Implementacao Controllers da API

---
+++ Core/CleanArchitecture.Application/Services/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CleanArchitecture.Application.Services;
+    public static class ServiceExtensions
+    {
+        public static void ConfigureApplicationApp(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(cfg =>
+                    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,25 +1,34 @@
+using CleanArchitecture.Persistence.Context;
+using CleanArchitecture.Persistence;
+using CleanArchitecture.Application.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigurePersistenceApp(builder.Configuration);
+builder.Services.ConfigureApplicationApp();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+CreateDatabase(app);
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseAuthorization();
-
+app.UseCors();
 app.MapControllers();
-
 app.Run();
+
+static void CreateDatabase(WebApplication app)
+{
+    var serviceScope = app.Services.CreateScope();
+    var dataContext = serviceScope.ServiceProvider.GetService<AppDbContext>();
+    //Se o banco de dados existir e possuir alguma tabela -> nao faz nenhuma acao
+    //Se o banco de dados existir mas nao tem nenhuma tabela -> vai criar o esquema definido na entidade no modelo de dominio
+    //Se nao existir banco de dados ele vai criar os dois
+    dataContext?.Database.EnsureCreated();
+}
