a4e11ee4db104c88cd61edf0d8c21c7ca157ea11
Gabriel <gabrielteles931@gmail.com>
2024-01-11 10:51:05 -0300

    1 parte da 05

--- Core/CleanArchitecture.Application/Services/ServiceExtensions.cs
+++ Core/CleanArchitecture.Application/Services/ServiceExtensions.cs
@@ -1,6 +1,13 @@
-﻿namespace CleanArchitecture.Application;
+﻿using System.Reflection;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 
-public class ServiceExtensions
+namespace CleanArchitecture.Application.Services;
+
+public static class ServiceExtensions
 {
-
+    public static void ConfigureApplicationApp(this IServiceCollection services){
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
