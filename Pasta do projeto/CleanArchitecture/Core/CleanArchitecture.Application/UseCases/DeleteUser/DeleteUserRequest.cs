411ac3a91ac7739103eedced3236f7accaed407f
Caua-Clemente <cauaclementelima@gmail.com>
2024-01-14 18:02:31 -0300

    Finalizando a última aula da playlist do Macoratti

---
+++ Pasta do projeto/CleanArchitecture/Core/CleanArchitecture.Application/UseCases/DeleteUser/DeleteUserRequest.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.Application.UseCases.DeleteUser;
+
+public sealed record DeleteUserRequest(Guid Id) : IRequest<DeleteUserResponse>;
--- Pasta do projeto/CleanArchitecture/Presentation/CleanArchitecture.API/Controllers/UsersController.cs
+++ Pasta do projeto/CleanArchitecture/Presentation/CleanArchitecture.API/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 ﻿using CleanArchitecture.Application.UseCases.CreateUser;
 using CleanArchitecture.Application.UseCases.GetAllUser;
+using CleanArchitecture.Application.UseCases.UpdateUser;
+using CleanArchitecture.Application.UseCases.DeleteUser;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -26,6 +28,28 @@ public class UsersController : ControllerBase
     public async Task<ActionResult<CreateUserResponse>> Create(CreateUserRequest request, CancellationToken cancellationToken)
     {
         var response = await _mediator.Send(request, cancellationToken);
+        return Ok(response);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<UpdateUserResponse>>Update(Guid id, UpdateUserRequest request, CancellationToken cancellationToken)
+    {
+        if(id != request.Id)
+            return BadRequest();
+
+        var response = await _mediator.Send(request, cancellationToken);
+        return Ok(response);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(Guid? id, CancellationToken cancellationToken)
+    {
+        if(id is null)
+            return BadRequest();
+
+        var deleteUserRequest = new DeleteUserRequest(id.Value);
+
+        var response = await _mediator.Send(deleteUserRequest, cancellationToken);
         return Ok(response);
     }
 }
