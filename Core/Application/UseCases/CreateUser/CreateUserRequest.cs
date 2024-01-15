8335052f460cdd1c732e2dc92c73c4b2f94909fb
NathalieLima <nathalielima3@gmail.com>
2024-01-14 23:22:12 -0300

    Término da parte 04: implementação de caso de uso de User

---
+++ Core/Application/UseCases/CreateUser/CreateUserRequest.cs
@@ -0,0 +1,9 @@
+using MediatR;
+
+namespace CleanArchitecture.Application.UseCases.CreateUser
+{
+    public sealed record CreateUserRequest(
+        string Email, string Name) :
+         IRequest<CreateUserResponse>;
+
+}
