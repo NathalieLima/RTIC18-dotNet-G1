c293527e8deb812abec851f3056eef78aa8492b5
NathalieLima <nathalielima3@gmail.com>
2024-01-14 22:16:41 -0300

    Término da parte 03: desenvolvimento da camada Infrastructure, configuração em Presentation e pequena correção em BaseEntity

--- Core/Domain/Entities/BaseEntity.cs
+++ Core/Domain/Entities/BaseEntity.cs
@@ -5,5 +5,5 @@ public abstract class BaseEntity
     public Guid Id { get; set; }
     public DateTimeOffset DateCreated { get; set; }
     public DateTimeOffset? DateUpdated { get; set; }
-    public DateTimeOffset? DataDeleted { get; set; }
+    public DateTimeOffset? DateDeleted { get; set; }
 }
---
+++ Infrastructure/Persistence/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Persistence.Context;
+
+public class AppDbContext : DbContext
+{
+      public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+      {
+      }
+   
+      public DbSet<User> Users { get; set; }
+}
