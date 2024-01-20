562cc7f138181fd43c2f80532476bbf7842c78f5
Caua-Clemente <cauaclementelima@gmail.com>
2024-01-20 20:09:22 -0300

    Criando Endereco e Perfil

---
+++ ResTIConnect/ResTIConnect.Domain/Entities/Endereco.cs
@@ -0,0 +1,17 @@
+﻿using ResTIConnect.Domain.Common;
+
+namespace ResTIConnect.Domain.Entities
+{
+    public class Endereco
+    {
+        public int EnderecoId { get; }
+        public required string  Logradouro { get; set; }
+        public required string  Numero { get; set; }  
+        public required string  Cidade { get; set; }
+        public string?  Complemento { get; set; }
+        public required string  Bairro { get; set; }
+        public required string  Estado { get; set; }
+        public required string  Cep { get; set; }
+        public required string  Pais { get; set; }
+    }
+}
---
+++ ResTIConnect/ResTIConnect.Domain/Entities/Perfil.cs
@@ -0,0 +1,11 @@
+﻿using ResTIConnect.Domain.Common;
+
+namespace ResTIConnect.Domain.Entities
+{
+    public class Perfil
+    {
+        public int PerfilId { get; }
+        public required string  Descricao { get; set; }
+        public required string  Permissoes { get; set; }       
+    }
+}
