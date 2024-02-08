a081f4d59306d403bdca65c801b20caca1448285
Lorena Andrade <lorenaandrade@gmail.com>
2024-02-08 10:40:06 -0300

    interface de User

--- ResTIConnect.Application/Services/Interfaces/IUserService.cs
+++ ResTIConnect.Application/Services/Interfaces/IUserService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ResTIConnect.Application.InputModels;
 using ResTIConnect.Application.ViewModels;
 
 namespace ResTIConnect.Application.Services.Interfaces
@@ -12,7 +13,13 @@ namespace ResTIConnect.Application.Services.Interfaces
         
       public List<UserViewModel> GetAll();
       public UserViewModel? GetById(int id);
+      public List<UserViewModel> GetByEnderecoUF(string uf);
+      public List<UserViewModel> GetBySistemaId(int sistemaId);
+      public List<UserViewModel> GetByPerfilId(int perfilId);
       public int Create(NewUserInputModel user);
+
+      public void AdicionaSistemaAoUser(int userId, int sistemaId);
+      public void AdicionaPerfilAoUser(int userId, int sistemaId);
       public void Update(int id, NewUserInputModel user);
       public void Delete(int id);
     }
