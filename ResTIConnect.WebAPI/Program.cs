57f1a474705a58d42ce838d25a6dca295eb88e31
AlanPrates <alan_nz@live.com>
2024-02-09 13:15:22 -0300

    'ResTIConnect API' para a documentação do Swagger

--- ResTIConnect.WebAPI/Program.cs
+++ ResTIConnect.WebAPI/Program.cs
@@ -1,21 +1,30 @@
-
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using ResTIConnect.Infra.Data.Context;
+using Microsoft.OpenApi.Models;
+using ResTIConnect.Application.Services.Interfaces;
+using ResTIConnect.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ResTIConnectContext>(options => {
     var connectionString = builder.Configuration.GetConnectionString("ResTIConnect");
-
     var serverVersion = ServerVersion.AutoDetect(connectionString);
-
-      options.UseMySql(connectionString, serverVersion);
+    options.UseMySql(connectionString, serverVersion);
 });
+
+builder.Services.AddScoped<IPerfilService, PerfilService>();
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ResTIConnect API", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -23,7 +32,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ResTIConnect API"));
 }
 
 app.UseHttpsRedirection();
