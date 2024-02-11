ae022a636a22fc0088d851089346fc90a1dbaf88
Daniel Oliveira <danieloliveira.s558@gmail.com>
2024-02-11 19:54:05 -0300

    adicionando camada insfraestruture

--- TechMed.Dommain/Entities/Atendimento.cs
+++ TechMed.Dommain/Entities/Atendimento.cs
@@ -2,7 +2,11 @@ namespace TechMed.Dommain.Entities;
 public class Atendimento : BaseEntity
 {
     public int AtendimentoId { get; set; }
-    public DateTime DataHora { get; set; }
+   
+    public DateTimeOffset DataHoraInicio { get; set; }
+    public string? SuspeitaInicial { get; set; }
+    public DateTimeOffset DataHoraFim { get; set; }
+    public string? Diagnostico { get; set; }
     public int MedicoId { get; set; }
     public required Medico Medico { get; set; }
     public int PacienteId { get; set; }
---
+++ TechMed.Infrastructure/Configurations/AtendimentoConfigurations.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TechMed.Core.Entities;
+
+namespace TechMed.Infrastructure.Configurations;
+
+public class AtendimentoConfigurations : IEntityTypeConfiguration<Atendimento>
+{
+    public void Configure(EntityTypeBuilder<Atendimento> builder)
+    {
+        //Tabela configurations
+        builder.ToTable("Atendimentos").HasKey(a => a.AtendimentoId);
+        
+        //collum type configurations
+        builder.Property(a => a.SuspeitaInicial).HasColumnType("longtext");
+        builder.Property(a => a.Diagnostico).HasColumnType("longtext");
+     
+
+        //Tables relacional
+        builder
+        .Property(a => a.MedicoId).IsRequired();
+        builder
+        .Property(a => a.PacienteId).IsRequired();
+        
+        //FK
+        builder
+        .HasOne(a => a.Medico).WithMany(a => a.Atendimentos).HasForeignKey(a => a.MedicoId);
+        builder
+        .HasOne(a => a.Paciente).WithMany(a => a.Atendimentos).HasForeignKey(a => a.PacienteId);
+
+    }   
+}
