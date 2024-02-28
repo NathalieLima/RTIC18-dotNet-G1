68fabe1a44a199a04ebb9adfc55bcbff90959947
LobinhoTroll <gabrielteles931@gmail.com>
2024-02-27 21:02:31 -0300

    Implementaando a autenticação via Jwt a partir do Login.

--- ResTIConnect.Application/Services/LoginService.cs
+++ ResTIConnect.Application/Services/LoginService.cs
@@ -1,40 +1,48 @@
 using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ResTIConnect.Application.InputModels;
 using ResTIConnect.Application.Services.Interfaces;
 using ResTIConnect.Application.ViewModels;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using ResTIConnect;
+using Microsoft.Extensions.Configuration;
+using System.Security.Cryptography.X509Certificates;
 
-namespace ResTIConnect.Application.Services
+namespace ResTIConnect.Application.Services;
+
+public class LoginService : ILoginService
 {
-    public class LoginService : ILoginService
+    private readonly IUserService _userService; //Contrado com os metodos de acesso aos usuarios
+    private readonly IConfiguration _configuration; //Contrato com os metodos para acesso ao json da WebApi 
+
+    public LoginService(IUserService userService)
     {
-
-        private readonly IUserService _userService;
-        public LoginService(IUserService userService)
-        {
-            _userService = userService;
-        }
-        public LoginViewModel? Authenticate(LoginInputModel login)
-        {
-
-            string _token = "";
-
-            if (_userService.AutenticateUser(login.Email, login.Password))
-            {
-                _token = "usuario logado com sucesso.";//TODO gerar o token JWT aqui
-            }
-            
-            if (_token != "")
-            {
-                return new LoginViewModel
-                {
-                    Username = login.Email,
-                    Token = _token
-                };
-            }
-            return null;
-        }
+        _userService = userService;
     }
-}
+    public LoginViewModel? Authenticate(LoginInputModel login)
+    {
+        
+        string _token = ""; //token inicialmente vazio
+
+        if (_userService.AutenticateUser(login.Email, login.Password))
+        {
+            var authService = new AuthService(_configuration); // Instancia o serviço de autenticação
+            _token = authService.GenerateJwtToken(login.Email, login.Email); //Gera o token e atribui o valor dele
+        }
+        if (_token != "")//se tudo der certo retorna um novo login com o token
+        {
+            return new LoginViewModel
+            {
+                Username = login.Email,
+                Token = _token
+            };
+        }
+        return null;
+    }
+}  
+    
