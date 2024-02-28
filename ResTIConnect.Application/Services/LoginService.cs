b72d0f1b16971e267eff841c31b0defae09b3a3b
LobinhoTroll <gabrielteles931@gmail.com>
2024-02-28 09:17:06 -0300

    consertando erro na geração do Jwt

--- ResTIConnect.Application/Services/LoginService.cs
+++ ResTIConnect.Application/Services/LoginService.cs
@@ -1,29 +1,19 @@
-using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ResTIConnect.Application.InputModels;
 using ResTIConnect.Application.Services.Interfaces;
 using ResTIConnect.Application.ViewModels;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using ResTIConnect;
-using Microsoft.Extensions.Configuration;
-using System.Security.Cryptography.X509Certificates;
+
 
 namespace ResTIConnect.Application.Services;
 
 public class LoginService : ILoginService
 {
     private readonly IUserService _userService; //Contrado com os metodos de acesso aos usuarios
-    private readonly IConfiguration _configuration; //Contrato com os metodos para acesso ao json da WebApi 
-
-    public LoginService(IUserService userService)
-    {
-        _userService = userService;
-    }
+    private readonly IAuthService _authService; //Contrato com os metodos para acesso ao json da WebApi 
+    public LoginService(IUserService userService, IAuthService authService)
+        {
+            _userService = userService;
+            _authService = authService;
+        }
     public LoginViewModel? Authenticate(LoginInputModel login)
     {
         
@@ -31,8 +21,7 @@ public class LoginService : ILoginService
 
         if (_userService.AutenticateUser(login.Email, login.Password))
         {
-            var authService = new AuthService(_configuration); // Instancia o serviço de autenticação
-            _token = authService.GenerateJwtToken(login.Email, login.Email); //Gera o token e atribui o valor dele
+            _token = _authService.GenerateJwtToken(login.Email, login.Email); //Gera o token e atribui o valor dele
         }
         if (_token != "")//se tudo der certo retorna um novo login com o token
         {
